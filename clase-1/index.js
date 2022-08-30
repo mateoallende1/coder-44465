fc6725d5f7ae61222469bbf93a5576e900ed25e7
Mateo Allende <mateoallende1@gmail.com>
2022-08-30 18:40:11 -0300

    js2

--- clase-1/index.js
+++ clase-1/index.js
@@ -1,7 +1,44 @@
 //forma NO recomendada de declarar variables
-var nombre = "camila";
+// var nombre = "camila";
 
 
 //declaracion de variable = asignacion de variable
-let edad = 45;
-const ALTURA = 180; 
+// let edad = 45;
+// const ALTURA = 180;  //constantes en mayuscula
+// let numeroUnoBonito;
+
+// console.log("aca la altura", ALTURA);
+
+// let numeroUno = 5;
+// let numeroDos = 10;
+
+// let suma = numeroUno + numeroDos;
+// let multiplicacion = numeroUno * numeroDos;
+// let resta = numeroUno - numeroDos;
+// let division = numeroUno / numeroDos;
+// console.log(suma);
+// console.log(multiplicacion);
+// console.log(resta);
+// console.log(division);
+
+//let nombre = 'andres';
+//let saludo = 'hola';
+//let mensaje = saludo + " " + nombre + " todo bien?" //hace lo mismo que mensaje1
+//let mensaje1 = `${saludo} ${nombre} todo bien?`     //hace lo mismo que mensaje pero mas prolijo con comillas invertidas
+
+//console.log(mensaje);
+
+//prompt ("Ingrese su nombre"); ME APARECE UN CUADRADO PARA INGRESAR TEXTO EN EL LIVE SERVER
+
+//let nombre = prompt ("Ingrese su nombre");
+//console.log(nombre);
+//alert(nombre);
+
+// let numero1 = parseInt(prompt("Ingrese un numero 1"));
+// let numero2 = parseInt(prompt("Ingrese un numero 2"));
+
+// let suma = numero1 + numero2;
+// console.log(suma);
+// alert(suma);
+
+console.log(parseFloat("5.5"));
