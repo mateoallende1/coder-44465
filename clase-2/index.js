4f9af7d502f9408f569b71921fed49b34aef596f
Mateo Allende <mateoallende1@gmail.com>
2022-08-30 19:57:28 -0300

    second class JS

---
+++ clase-2/index.js
@@ -0,0 +1,116 @@
+// Estructura condicional if
+
+// if (condicion) {
+//     codigo a ejecutar en el caso que la condicion sea verdadera
+// }
+
+// let nombre = "mario";
+
+// if(nombre === "mariano") {
+//     console.log("Hola Mariano");
+// } 
+
+// if (0) {
+//     console.log("hola");
+// }
+// console.log("chao");   CON 0 DA FALSE
+
+// if (2) {
+//     console.log("hola");
+// }
+// console.log("chao");
+
+
+// let nombre = prompt("ingrese un nombre");
+
+// if (nombre === "camila") {
+//     alert("Hola Camila")
+// }
+
+//Estructura if else
+// if (condicion){
+//     codigo a ejecutrar en caso de que la condicion sea verdadera
+// }
+// else{
+//     condigo a ejecutar en caso de que la condicion no sea verdadera
+// }
+
+// let numero = 11;
+
+// if (numero === 10){
+//     console.log("eres el 10");
+// }
+// else {
+//     console.log("no eres el 10");
+// }
+
+//Estructura del if else if else
+// if(condicion 1){
+//     codigo a ejecutar cuando la condicion 1 es verdadera
+// }
+// else if (condicion 2){
+//     codigo a ejecutar cuando la condicion 2 es verdadera
+// }
+// else {
+//     codigo a ejecutar cuando ninguna de las dos condiciones es verdadera
+// }
+
+// let numero = 20;
+
+// if (numero === 20){
+//     console.log("eres el 20");
+// }
+// else if (numero === 23){
+//     console.log("eres el 23");
+// }
+// else {
+//     console.log("no eres ninguno de los 2");
+// }
+
+// console.log("final");
+
+// let nombre = prompt ("ingrese el nombre");
+
+// if (nombre === ""){
+//     alert("No igresaste nada")
+// }
+// else{
+//     alert(`Bienvenido ${nombre}`)
+// }
+
+// let respuesta = prompt("terminaste la tarea?")
+
+// if (respuesta === "si"){
+//     alert("puedes salir a jugar")
+// }
+// else {
+//     alert("no puedes salir a jugar")
+// }
+
+// let precio = 99;
+
+// if (precio < 20){
+//     console.log("precio menor a 20");
+// }
+// else if (precio < 50) {
+//     console.log('precio menor a 50');
+// }
+// else if (precio < 100){
+//     console.log("precio menos a 100");
+// }
+// else{
+//     console.log("precio mayor a 100");
+// }
+
+// let edad = parseInt(prompt("Ingrese su edad"));
+
+// if (edad < 15){
+//     alert("no puedes entrar a la fiesta");
+// }
+// else if (edad <= 18) {
+//     alert("puede entrar con un adulto");
+// }
+// else{
+//     alert("puede entrar");
+// }
+
